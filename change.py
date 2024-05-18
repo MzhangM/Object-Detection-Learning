339e8acce155c36c03fe173d6aadb8c7587ccb00
Qzhang <132986832+MzhangM@users.noreply.github.com>
2024-05-18 17:39:48 +0800

    Update change.py

--- change.py
+++ change.py
@@ -6,30 +6,30 @@ import numpy as np
 def split_dataset(data_dir, train_ratio=0.7, val_ratio=0.2, test_ratio=0.1):
     assert np.isclose(train_ratio + val_ratio + test_ratio, 1.0)
 
-    # 获取所有图片文件的完整路径
+    # get all images path
     image_paths = [os.path.join(data_dir, f) for f in os.listdir(data_dir) if f.endswith('.jpg')]
     # 确保每张图片都有一个对应的txt文件
     data_paths = [(img_path, img_path.replace('.jpg', '.txt')) for img_path in image_paths if
                   os.path.exists(img_path.replace('.jpg', '.txt'))]
 
-    # 打乱数据顺序
+    # disorder dataset
     np.random.shuffle(data_paths)
 
-    # 计算分割点
+    # calculate the split point
     total_files = len(data_paths)
     train_end = int(total_files * train_ratio)
     val_end = train_end + int(total_files * val_ratio)
 
-    # 分割数据集
+    # separate dataset
     train_files = data_paths[:train_end]
     val_files = data_paths[train_end:val_end]
     test_files = data_paths[val_end:]
 
-    # 创建存储目录
+    #  create a storage dir
     for folder in ['train', 'val', 'test']:
         os.makedirs(os.path.join(data_dir, folder), exist_ok=True)
 
-    # 移动文件
+    # move files
     def move_files(files, folder):
         for img_path, txt_path in files:
             shutil.move(img_path, os.path.join(data_dir, folder, os.path.basename(img_path)))
